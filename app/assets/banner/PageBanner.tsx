75a82a55ebb3ed138a39abf6943f21148ce5e9ad
Yagya Adh <yagyaadhikari02@gmail.com>
2024-10-08 08:03:07 +0545

    page banner  image componet attribute re-write

--- app/assets/banner/PageBanner.tsx
+++ app/assets/banner/PageBanner.tsx
@@ -10,10 +10,10 @@ const PageBanner = () => {
     <>
       <div className="relative lg:text-center flex flex-col text-white justify-center items-center p-10 py-40">
         <div className="hidden lg:flex absolute top-10 left-20 overflow-hidden rounded-full">
-          <Image src={personOne} all="home_" className="size-48" />
+          <Image src={personOne} alt="home_" className="size-48" />
         </div>
         <div className="hidden lg:flex absolute bottom-10 right-20 overflow-hidden rounded-full">
-          <Image src={personTwo} all="home_" className="size-48" />
+          <Image src={personTwo} alt="home_" className="size-48" />
         </div>
 
         <div className="flex flex-col items-center md:text-center  justify-center">
