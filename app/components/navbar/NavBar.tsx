2fed9727b91e50a88910aefa839c17244c93d9b7
Yagya Adh <yagyaadhikari02@gmail.com>
2024-10-17 18:20:02 +0545

    create: navbar for small screen just create

--- app/components/navbar/NavBar.tsx
+++ app/components/navbar/NavBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import ButtonPills from "../button/ButtonPills";
-import { Bars3Icon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Modal from "../modal/Modal";
 import MainLogo from "../mainLogo/MainLogo";
 import Link from "next/link";
@@ -20,15 +20,17 @@ const routes: Iroutes[] = [
 
 const NavBar = () => {
   const [cartOpen, setCartOpen] = useState(false);
-
+  const [open, setOpen] = useState(false);
   const handelCartOpen = () => {
     setCartOpen(!cartOpen);
   };
-
+  const handleOpenNavBar = () => {
+    return setOpen(!open);
+  };
   return (
     <>
       <nav>
-        <div className="rounded-full bg-productHives-mainBackground z-10 border border-transparent shadow-2xl flex justify-between items-center max-w-screen-2xl p-2 px-4 mx-auto">
+        <div className="z-40 rounded-full bg-productHives-mainBackground   border border-transparent shadow-2xl flex justify-between items-center max-w-screen-2xl p-2 px-4 mx-auto">
           <MainLogo logo="nav" />
           <div className="flex justify-between items-center bg-transparent rounded-full">
             <ul className="hidden lg:flex items-center">
@@ -53,10 +55,38 @@ const NavBar = () => {
               <span className="text-white lg:hidden" onClick={handelCartOpen}>
                 Cart (0 )
               </span>
-
-              <Bars3Icon className="size-10 text-green-700 lg:hidden" />
+              {open ? (
+                <XMarkIcon
+                  onClick={handleOpenNavBar}
+                  className="size-10 text-productHives-buttonColor lg:hidden"
+                />
+              ) : (
+                <Bars3Icon
+                  onClick={handleOpenNavBar}
+                  className="size-10 text-productHives-buttonColor lg:hidden"
+                />
+              )}
             </div>
           </div>
+        </div>
+        <div
+          className={` z-10 ${
+            !open
+              ? " -translate-y-96 transition-all ease-in-out duration-500 bg-none overflow-hidden"
+              : "translate-y-1 transition-all ease-in-out duration-500 py-10  rounded-2xl"
+          } `}
+        >
+          <ul className="flex flex-col py-4 lg:hidden rounded-2xl bg-white text-productHives-mainBackground">
+            {routes?.map((linkList) => (
+              <Link href={linkList.path} key={linkList.id}>
+                <li className="text-xl ps-5 py-2">{linkList.name}</li>
+              </Link>
+            ))}
+
+            <div>
+              <ButtonPills text="Buy Template" />
+            </div>
+          </ul>
         </div>
       </nav>
       {cartOpen && (
